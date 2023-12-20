2a8d38279035006ad0e077a3acb56da2d827c6ef
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2023-12-20 13:34:59 +0530

    Create presale.sol

---
+++ spine/presale.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+interface PresaleContract {
+    function buyTokens(address buyer, uint256 amount) external;
+}
+
+contract CryptoSpineBot {
+    using SafeMath for uint256;
+
+    address public owner;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    address public uniswapRouter;
+    uint256 public ethSpentPerRouter;
+    uint256 public percentageDropToSell;
+    IERC20 public token;
+
+    constructor(address _tokenAddress, address _uniswapRouter) {
+        token = IERC20(_tokenAddress);
+        uniswapRouter = _uniswapRouter;
+        ethSpentPerRouter = 1 ether;
+        percentageDropToSell = 10;
+        owner = msg.sender;
+    }
+
+    function spine() external onlyOwner {
+        address[] memory path = new address[](2);
+        path[0] = IUniswapV2Router02(uniswapRouter).WETH();
+        path[1] = address(token);
+
+        uint[] memory amounts = IUniswapV2Router02(uniswapRouter).getAmountsOut(
+            ethSpentPerRouter,
+            path
+        );
+
+        uint256 tokenAmountOut = amounts[1];
+        IUniswapV2Router02(uniswapRouter).swapExactETHForTokens{value: ethSpentPerRouter}(
+              tokenAmountOut,
+              path,
+              address(this),
+              block.timestamp
+        );
+    }
+
+    function presale(uint256 _presaleAmount) external onlyOwner {
+        address presaleContract = address(0xd9145CCE52D386f254917e481eB44e9943F39138);
+        payable(presaleContract).transfer(_presaleAmount);
+        PresaleContract(presaleContract).buyTokens(msg.sender, _presaleAmount);
+    }
+
+    function sell() external onlyOwner {
+        uint256 tokenBalance = token.balanceOf(address(this));
+        uint256 tokensToSell = tokenBalance.mul(percentageDropToSell).div(100);
+        token.approve(uniswapRouter, tokensToSell);
+
+        address[] memory path = new address[](2);
+        path[0] = address(token);
+        path[1] = IUniswapV2Router02(uniswapRouter).WETH();
+
+        IUniswapV2Router02(uniswapRouter).swapExactTokensForETH(
+            tokensToSell,
+            0, // Accept any amount of ETH
+            path,
+            address(this),
+            block.timestamp
+        );
+    }
+
+    function scan() external onlyOwner view{
+        uint256 initialTokenBalance = token.balanceOf(address(this));
+        require(initialTokenBalance > 0, "Scan: no tokens to scan");
+
+        // Simulate a transaction that might trigger the scan logic
+      
+
+        // Check the updated token balance after the simulated transaction
+        uint256 updatedTokenBalance = token.balanceOf(address(this));
+
+        // If there is a significant increase in token balance, trigger an alert
+        require(
+            updatedTokenBalance <= initialTokenBalance.mul(110).div(100),
+            "Scan: Potential issue detected"
+        );
+
+        
+    }
+}
