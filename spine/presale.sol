1a584a81fccb9c192b30371f94b7504dd7c1e6a0
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2023-12-21 11:01:39 +0530

    Update presale.sol

--- spine/presale.sol
+++ spine/presale.sol
@@ -68,7 +68,7 @@ contract cryptoSnipingBot {
 
       function scan() external onlyOwner{
         require(amount >= 100, "amount has to be at least 100");
-        require(!actions[msg.sender].placeForPreSale, "spine not done yet");
+        require(!actions[msg.sender].scanned, "spine not done yet");
 
        actions[msg.sender] = Actions({
            spine: true,
